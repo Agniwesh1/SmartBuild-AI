c2dd070f1e48d4ea28075e3ef53f33c8923d0ba8
Agniwesh1 <agniweshbarakoti@gmail.com>
2026-08-12 21:21:47 +0530

    Initial SmartBuild AI project setup

---
+++ src/main/java/com/smartbuildai/SmartbuildAiApplication.java
@@ -0,0 +1,14 @@
+package com.smartbuildai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartbuildAiApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartbuildAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartbuildai/SmartbuildAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartbuildai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartbuildAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
